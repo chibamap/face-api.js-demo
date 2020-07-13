596cd9bc6208f12161cbc6342597a2b667f9e0c4
hogehoge-banana <tsuk.316.banana@gmail.com>
2020-07-14 01:30:21 +0900

    add face api demo

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,14 +45,17 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    //'@nuxtjs/eslint-module',
   ],
+  eslint: {
+    // fix: true,
+  },
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    //'@nuxtjs/axios',
   ],
   /*
    ** Axios module configuration
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,89 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        face-api.js-demo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  div.container
+    video(ref="video" width="720" height="560" autoplay muted)
 </template>
 
 <script>
-export default {}
+import * as faceapi from 'face-api.js'
+
+const MODEL_URL = '/models'
+
+export default {
+  data: () => {
+    return {
+      videoSize : null,
+      canvas: null
+    }
+  },
+  mounted() {
+    this.$refs.video.addEventListener('play', this.onVideoPlay);
+    this.loadModels().then(this.askUserToStart).then(this.startVideo);
+  },
+  methods: {
+    loadModels() {
+      return Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]);
+    },
+    askUserToStart() {
+      return new Promise((resolve, reject) => {
+        if (confirm('videoを利用します。')) {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    },
+    startVideo() {
+      navigator.getUserMedia(
+        { video: {} },
+        stream => this.$refs.video.srcObject = stream,
+        err => console.error(err)
+      )
+    },
+    onVideoPlay() {
+      console.log('on video play');
+      const videoSize = {
+        width: this.$refs.video.width,
+        height: this.$refs.video.height
+      };
+      const canvas = faceapi.createCanvasFromMedia(this.$refs.video);
+      this.$el.append(canvas);
+      faceapi.matchDimensions(canvas, videoSize);
+
+      setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(
+            this.$refs.video,
+            new faceapi.TinyFaceDetectorOptions()
+        )
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+        const resizedDetections = faceapi.resizeResults(detections, videoSize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        const minProbability = 0.05;
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections, minProbability);
+      }, 100);
+    }
+  },
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+video, canvas {
+  position: absolute;
 }
 </style>
